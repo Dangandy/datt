bd92a7ba01ee396dd9054b970412ed5002fe22b6
Andy <andy@unyte.com>
2020-12-18 07:50:17 -0500

    added menu icon

---
+++ src/components/MenuIcon.js
@@ -0,0 +1,100 @@
+/**
+ * Menu icon forked from: https://www.jomor.design/
+ */
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+
+const MenuIconStyles = styled.a`
+  position: fixed;
+  top: 50px;
+  right: 50px;
+  width: 58px;
+  height: 58px;
+  border-radius: 50%;
+  cursor: pointer;
+
+  ${({ hover, toggle }) =>
+    hover || toggle
+      ? css`
+          border: 2px solid var(--red);
+          transition: 0.3s ease-out;
+        `
+      : css`
+          border: 2px solid var(--green);
+          transition: 0.3s ease-out;
+        `}
+
+  & .line {
+    width: 20px;
+    height: 2px;
+    margin-top: 8px;
+    margin-bottom: 8px;
+    ${({ hover, toggle }) =>
+      hover || toggle
+        ? css`
+            background-color: var(--red);
+            transition: 0.3s ease-out;
+          `
+        : css`
+            background-color: var(--green);
+            transition: 0.3s ease-out;
+          `}
+  }
+
+  & .line1 {
+    z-index: 1;
+    ${({ hover }) =>
+      hover &&
+      css`
+        transform: rotateZ(180deg);
+        margin: 0;
+        transition: 0.3s ease-out;
+      `}
+    ${({ toggle }) =>
+      toggle &&
+      css`
+        transform: translate3d(0.5px, 2px, 5px) rotateZ(45deg);
+        margin: 0;
+        transition: 0.3s ease-out;
+      `}
+  }
+  & .line2 {
+    z-index: 2;
+    ${({ hover }) =>
+      hover &&
+      css`
+        /* transform: translate3d(0px, -8px, 0px) rotateZ(270deg); */
+        transform: rotateZ(270deg);
+        margin: 0;
+        transition: 0.3s ease-out;
+      `}
+    ${({ toggle }) =>
+      toggle &&
+      css`
+        transform: rotateZ(135deg);
+        margin: 0;
+        transition: 0.3s ease-out;
+      `}
+  }
+  @media (max-width: 420px) {
+    display: grid;
+    place-content: center;
+  }
+  display: none;
+`;
+
+export default function MenuIcon({ toggle, setToggle }) {
+  const [hover, setHover] = useState(false);
+  return (
+    <MenuIconStyles
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      onClick={() => setToggle(!toggle)}
+      toggle={toggle}
+      hover={hover}
+    >
+      <div className="line line1" />
+      <div className="line line2" />
+    </MenuIconStyles>
+  );
+}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { FadeIn } from '../styles/FadeIn';
+import MenuIcon from './MenuIcon';
 
-const NavStyles = styled.nav`
+const NavGrid = styled.nav`
   width: 100%;
-  display: grid;
   place-content: center;
   margin-top: 6rem;
 
@@ -27,22 +27,39 @@ const NavStyles = styled.nav`
       }
     }
   }
+
+  @media (max-width: 420px) {
+    display: none;
+  }
+  display: grid;
+`;
+
+const Container = styled.div`
+  width: 100%;
+`;
+
+const NavBlock = styled.nav`
+  display: none;
 `;
 
 export default function Navbar() {
+  const [toggle, setToggle] = useState(false);
   return (
-    <NavStyles>
-      <ul className="nav-container">
-        <li>
-          <FadeIn>About</FadeIn>
-        </li>
-        <li>
-          <FadeIn>Projects</FadeIn>
-        </li>
-        <li>
-          <FadeIn>Contact</FadeIn>
-        </li>
-      </ul>
-    </NavStyles>
+    <Container>
+      <NavGrid>
+        <ul className="nav-container">
+          <li>
+            <FadeIn>About</FadeIn>
+          </li>
+          <li>
+            <FadeIn>Projects</FadeIn>
+          </li>
+          <li>
+            <FadeIn>Contact</FadeIn>
+          </li>
+        </ul>
+      </NavGrid>
+      <MenuIcon setToggle={setToggle} toggle={toggle} />
+    </Container>
   );
 }
